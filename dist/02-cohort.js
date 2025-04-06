6ca12dda7cbdf944b435e96c2525e240684745dd
Piyushrathoree <meonpath008@gmail.com>
2025-04-07 01:43:41 +0530

    Add user update functions and event handling example in 02-cohort.js

--- dist/02-cohort.js
+++ dist/02-cohort.js
@@ -1,1 +1,24 @@
 "use strict";
+function updateUser(updateProps) {
+}
+function updateAnotherUser(updateProps) {
+}
+function updateUser2(updatedProps) { }
+const Config = {
+    endpoint: "adfadfa",
+    apiKey: "adfadsfa",
+};
+const users = {
+    asdfasd: 12,
+    asdfadfa: 45,
+};
+const newUser10 = new Map();
+newUser10.set("first", {
+    name: "piyush",
+    email: "piyush@123gmail.com",
+    password: "23d32",
+});
+const handleClick = (event) => {
+    console.log(`handling event ${event}`);
+};
+handleClick("mousemove");
